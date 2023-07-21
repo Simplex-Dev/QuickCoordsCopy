c01d52e9cae3a41e390a37c31e4317fc2e05bca9
Eli <eli.axel.ang@gmail.com>
2023-07-21 15:04:12 +0800

    Bumped version from 1.19 to 1.20

--- src/main/java/io/github/simplexdev/quickcoordscopy/config/QuickCoordsCopyConfig.java
+++ src/main/java/io/github/simplexdev/quickcoordscopy/config/QuickCoordsCopyConfig.java
@@ -7,5 +7,5 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config(name = "quickcoordscopy")
 public class QuickCoordsCopyConfig implements ConfigData {
     @Comment("The format of the string that gets copied. Use $x, $y and $z as placeholders.")
-    public String format = "Coordinates: $x, $y, $z";
+    public String format = "$x, $y, $z";
 }
